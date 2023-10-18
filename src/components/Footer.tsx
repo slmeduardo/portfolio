b56df30c5d37672e5da1f7575014baa2054b18c4
slmeduardo <itsmeeduardoc@gmail.com>
2023-10-18 18:07:52 -0300

    fix: footer redesigned

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,45 @@
-import Link from "next/link"
-import {
-  SiTwitter,
-  SiGithub,
-  SiLinkedin
-} from "react-icons/si"
+import Link from "next/link";
+import { SiTwitter, SiGithub, SiLinkedin } from "react-icons/si";
 
 export default function Footer() {
-return (
+  return (
     <footer className="mx-96 mt-32">
-        <p className="text-gray-400 text-sm font-bold">About Me</p>
-        <p className="dark:text-gray-200 text-sm mt-3 max-w-sm">I am Eduardo Cardoso, a <span className="font-bold">front-end developer</span> with a genuine passion for clean and modern design.</p>
+      <p className="text-gray-400 text-sm font-bold">About Me</p>
+      <div className="flex justify-between">
+        <p className="dark:text-gray-200 text-sm mt-3 max-w-sm">
+          I am Eduardo Cardoso, a{" "}
+          <span className="font-bold">front-end developer</span> with a genuine
+          passion for clean and modern design.
+        </p>
         <div className="flex gap-3 mt-5">
           <Link href={"https://twitter.com/slmeduardo"} target="_blank">
-            <SiTwitter className="text-black dark:text-white" size={25} cursor="pointer" />
+            <SiTwitter
+              className="text-black dark:text-white"
+              size={25}
+              cursor="pointer"
+            />
           </Link>
           <Link href={"https://github.com/slmeduardo"} target="_blank">
-            <SiGithub className="text-black dark:text-white" size={25} cursor="pointer" />
+            <SiGithub
+              className="text-black dark:text-white"
+              size={25}
+              cursor="pointer"
+            />
           </Link>
-          <Link href={"https://www.linkedin.com/in/eduardo-cardoso-46695a238/"} target="_blank">
-            <SiLinkedin className="text-black dark:text-white" size={25} cursor="pointer" />
+          <Link
+            href={"https://www.linkedin.com/in/eduardo-cardoso-46695a238/"}
+            target="_blank"
+          >
+            <SiLinkedin
+              className="text-black dark:text-white"
+              size={25}
+              cursor="pointer"
+            />
           </Link>
         </div>
+      </div>
       <div className="my-12 flex-grow border-t border-gray-400 opacity-50"></div>
       <p className="text-xs font-bold pb-5">© 2023, Eduardo Cardoso</p>
-      </footer>
-  )
+    </footer>
+  );
 }
